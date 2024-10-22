94fb7cc1b2ed717956d28bb6b28b2346f79b67da
evanstcom <zhenyakoks@gmail.com>
2024-10-22 11:19:52 +0300

    validate phone

--- practice/1/script.js
+++ practice/1/script.js
@@ -1,5 +1,4 @@
 const modal = document.querySelector("#modal");
-console.log(modal);
 const openModal = document
   .querySelector("#open-modal")
   .addEventListener("click", () => {
@@ -34,3 +33,28 @@ document.getElementById("clearButton").addEventListener("click", function () {
   preview.src = "";
   preview.style.display = "none";
 });
+
+const phoneNumber = document.getElementById("phoneNumber");
+
+const submitButton = document
+  .getElementById("send-form")
+  .addEventListener("click", sendForm);
+
+function sendForm() {
+  const submit = validatePhone();
+  if (!submit) {
+    alert(`Номер телефона невалидный`);
+  }
+}
+
+function validatePhone() {
+  const phoneRegex =
+    /^\+7\s?\d{3}\s?\d{3}\s?\d{2}\s?\d{2}$|^8\s?\(?\d{3}\)?\s?\d{3}\s?\d{2}\s?\d{2}$|^7\d{10}$/;
+  if (phoneRegex.test(phoneNumber.value)) {
+    console.log(`"${phoneNumber.value}" - Номер телефона валидный`);
+    return true;
+  } else {
+    console.log(`"${phoneNumber.value}" - Номер телефона невалидный`);
+    return false;
+  }
+}
